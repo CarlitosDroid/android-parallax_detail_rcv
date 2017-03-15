db90a4ca8a24ac32794dca0af8da6a1d39d7c1c6
Carlos Leonardo Camilo Vargas Huamán <carlosleonardovargashuaman@gmail.com>
2017-03-15 11:50:58 -0500

    wip(ExampleUnitTest): modified

--- app/src/test/java/com/carlitosdroid/parallaxdetailrcv/ExampleUnitTest.java
+++ app/src/test/java/com/carlitosdroid/parallaxdetailrcv/ExampleUnitTest.java
@@ -20,7 +20,7 @@ public class ExampleUnitTest {
     }
     @Test
     public void addition_isCorrect3() throws Exception {
-        assertEquals(2, 2 + 2);
+        assertEquals(4, 2 + 2);
     }
     @Test
     public void addition_isCorrect4() throws Exception {
